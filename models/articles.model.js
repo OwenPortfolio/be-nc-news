bc2bbe00f147dcdd1fec1a9fd245bb7cceb251cf
OwenPortfolio <owen.manby@gmail.com>
2022-05-17 17:03:34 +0100

    add join to db query

--- models/articles.model.js
+++ models/articles.model.js
@@ -3,7 +3,7 @@ const db = require('../db/connection.js');
 
 exports.selectArticleById = (article_id) => {
     return db
-    .query('SELECT articles.*,  (SELECT COUNT(*) FROM comments WHERE comments.article_id = articles.article_id) AS comment_count FROM articles WHERE article_id = $1;', [article_id])
+    .query(`SELECT articles.*, COUNT(comments.article_id) AS comment_count FROM articles JOIN comments ON articles.article_id = comments.article_id GROUP BY articles.article_id HAVING articles.article_id = $1;`, [article_id])
     .then((article) => {
         if(!article.rows[0]){
             return Promise.reject({
